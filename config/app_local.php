3535e200046d1e6e17d3a41cfdb798ef35c64c8d
crashbrtb <crashbrtb@gmail.com>
2025-05-22 01:57:09 -0300

    Update app_local.php

--- config/app_local.php
+++ config/app_local.php
@@ -37,7 +37,7 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'localhost',
+            'host' => 'counterdkw.mysql.dbaas.com.br',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
