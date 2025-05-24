4eb5611b9de6bb5628db672f1c2b6df8c5242d8e
Aircrash <clesiovilani@gmail.com>
2025-05-24 20:30:11 -0300

    0.1.7.1

--- src/Controller/StandardChestsController.php
+++ src/Controller/StandardChestsController.php
@@ -18,6 +18,12 @@ class StandardChestsController extends AppController
      *
      * @return \Cake\Http\Response|null|void Renders view
      */
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+    
+        $this->Authentication->allowUnauthenticated(['weights']);
+    }
     public function index()
     {
         $query = $this->StandardChests->find();
